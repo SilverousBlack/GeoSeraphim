bf4b96f26729406749633da1d71b1febf7c15b95
SilverousBlack <juliancalebsegundo@gmail.com>
2022-12-05 04:34:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# GeoSeraphim
+
+> Angels descend and survey the Earth
+
+A centralized package for interfacing with geographical data.
---
+++ src/Core/__init__.py
@@ -0,0 +1,22 @@
+# Pre Requisite Modules and Libraries
+
+## Math and Matrices
+
+import numpy as np
+import pandas as pd
+
+## Geographical Libraries
+
+import osgeo
+import rasterio as rio
+
+## Plotting
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## File System
+
+import pathlib as pl
+
+
